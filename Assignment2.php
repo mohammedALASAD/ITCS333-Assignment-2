9fe2fb7308e7d01538c05b08382dbcb0aa48d3a7
mohammedALASAD <alasadmohammed2003@gmail.com>
2024-11-25 13:16:51 +0300

    Create Assignment2.php
    Assignment descreption

---
+++ Assignment2.php
@@ -0,0 +1,3 @@
+<?php
+// this assignment will solve the the qustion, create a PHP application that retrieves and displays UOB student nationality data from the Bahrain Open Data Portal using their public API.
+?>
