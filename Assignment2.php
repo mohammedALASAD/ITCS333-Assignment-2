9e14ecff6dda126b290024d10cf4b29676c0b7fe
mohammedALASAD <alasadmohammed2003@gmail.com>
2024-11-25 13:21:35 +0300

    Update Assignment2.php
    Initialize cURL

--- Assignment2.php
+++ Assignment2.php
@@ -1,3 +1,8 @@
 <?php
 $URL = "https://data.gov.bh/api/explore/v2.1/catalog/datasets/01-statistics-of-students-nationalities_updated/records?where=colleges%20like%20%22IT%22%20AND%20the_programs%20like%20%22bachelor%22&limit=100";
+
+$CH = curl_init($URL);
+curl_setopt($CH, CURLOPT_RETURNTRANSFER, true);
+$response = curl_exec($CH);
+curl_close($CH);
 ?>
