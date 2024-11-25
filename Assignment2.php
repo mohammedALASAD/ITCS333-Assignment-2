9d166e257f28e792bd4be48b5f2e2194c8f82766
mohammedALASAD <alasadmohammed2003@gmail.com>
2024-11-25 13:19:20 +0300

    Update Assignment2.php
    taking data from the API

--- Assignment2.php
+++ Assignment2.php
@@ -1,3 +1,3 @@
 <?php
-// this assignment will solve the the qustion, create a PHP application that retrieves and displays UOB student nationality data from the Bahrain Open Data Portal using their public API.
+$URL = "https://data.gov.bh/api/explore/v2.1/catalog/datasets/01-statistics-of-students-nationalities_updated/records?where=colleges%20like%20%22IT%22%20AND%20the_programs%20like%20%22bachelor%22&limit=100";
 ?>
